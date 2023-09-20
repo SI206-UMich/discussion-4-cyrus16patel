6a81436cbf0eda9ab3b36ea6ffa020cebc8c9c5a
Cyrus Patel <cryptel@Miteshs-MacBook-Pro.local>
2023-09-20 18:50:48 -0400

    1st commit

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -46,18 +46,18 @@ class Rectangle():
     
 
 
-def main():
-    r = Rectangle(10, 10)
-    print(r)
-    print("Area:", r.area())
-    print("Perimeter:", r.perimeter())
-    print()
+    def main():
+        r = Rectangle(10, 10)
+        print(r)
+        print("Area:", r.area())
+        print("Perimeter:", r.perimeter())
+        print()
 
-    r = Rectangle(0, 10)
-    print(r)
-    print("Area:", r.area())
-    print("Perimeter:", r.perimeter())
-    print()
+        r = Rectangle(0, 10)
+        print(r)
+        print("Area:", r.area())
+        print("Perimeter:", r.perimeter())
+        print()
 
 if __name__ == "__main__":
-    main()
+        main()
